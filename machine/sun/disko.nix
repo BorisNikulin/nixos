db92bead785f96713c0b9d5d820411f7474d409a
Boris Nikulin <NikulinBE@gmail.com>
2025-05-15 23:53:02 -0700

    feat(disk): Add main pool, datasets, and drives to disko

--- machine/sun/disko.nix
+++ machine/sun/disko.nix
@@ -1,6 +1,26 @@
 { config, lib, pkgs, ... } :
 {
-  disko.devices = {
+  disko.devices = let
+    mkZfsDisk = pool: device: {
+        type = "disk";
+        inherit device;
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                inherit pool;
+              };
+            };
+          };
+        };
+    };
+    mkZfsFastDisk = mkZfsDisk "fast";
+    mkZfsMainDisk = mkZfsDisk "main";
+  in
+  {
     disk = {
       # Crucial MX500 500G
       crucialMx500500g1 = {
@@ -30,42 +50,16 @@
         };
       };
 
-      # Samsung 990 Pro 4T x2
-      samsung990Pro4t1 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-eui.002538414144adbf";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "fast";
-              };
-            };
-          };
-        };
-      };
-      samsung990Pro4t2 = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-eui.002538414144ae76";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "fast";
-              };
-            };
-          };
-        };
-      };
+      # Samsung 990 Pro 4T
+      samsung990Pro4t1 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144adbf";
+      samsung990Pro4t2 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144ae76";
 
-      # Ironwolf pro 18T x5 already existing raidz2
-      # ironwolfPro18t1 = {}; TODO
+      # Ironwolf pro 18T
+      ironwolfPro18t1 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bd498e";
+      ironwolfPro18t2 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e45ff430";
+      ironwolfPro18t3 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5ace6c3";
+      ironwolfPro18t4 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5f75a6c";
+      ironwolfPro18t5 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bc90ab";
 
     };
     zpool = {
@@ -144,6 +138,7 @@
           canmount = "off";
           mountpoint = "none";
           atime = "off";
+          xattr = "sa";
           compression = "lz4";
         };
         datasets = {
@@ -179,14 +174,88 @@
           };
         };
       };
+      
+      main = {
+        type = "zpool";
+        mode = {
+          topology = {
+            type = "topology";
+            vdev = [
+              {
+                mode = "raidz2";
+                members = [
+                  "ironwolfPro18t1"
+                  "ironwolfPro18t2"
+                  "ironwolfPro18t3"
+                  "ironwolfPro18t4"
+                  "ironwolfPro18t5"
+                ];
+              }
+            ];
+          };
+        };
+        options = {
+          ashift = "12";
+        };
+        rootFsOptions = {
+          canmount = "off";
+          mountpoint = "none";
+          atime = "off";
+          xattr = "sa";
+          compression = "zstd";
+        };
+        datasets = {
+          encrypted = {
+            type = "zfs_fs";
+            options = {
+              canmount = "off";
+              mountpoint = "none";
+              encryption = "aes-256-gcm";
+              keyformat = "hex";
+              keylocation = "file:///etc/zfs/key/main/encrypted.hex";
+              # Use to bootstrap initial creation during install when root does not exist
+              # keyformat = "passphrase";
+              # keylocation = "prompt";
+            };
+          };
+          share = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/mnt/main/share";
+              canmount = "on";
+              casesensitiveity = "insensitive";
+              acltype = "nfsv4";
+              aclmode = "restricted";
+            };
+          };
+          "share/public" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/mnt/main/share/public";
+              canmount = "on";
+            };
+          };
+          "share/public-write" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/mnt/main/share/public";
+              canmount = "on";
+              refquota = "1T";
+            };
+          };
+          media = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "/mnt/main/media";
+              canmount = "on";
+              acltype = "posix";
+            };
+          };
+        };
+      };
     };
   };
-  # };
 
-  # key for /nix needed for boot ultimately comes from the root dataset of fast
-  # fileSystems."${disko.devices.zpool.fast.mountpoint}".depends = [ "/etc/zfs/keys" ];
-  # fileSystems."/nix".depends = [ "/" ];
-  # fileSystems."${disko.devices.zpool.fast.datasets.nix.mountpoint}".depends = [ "/" ];
   boot.zfs.extraPools = [
     "fast"
     "main"
