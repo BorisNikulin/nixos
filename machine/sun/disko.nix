bcf48111b0192eb06872c5ba4aed6e86bd6c0415
Boris Nikulin <NikulinBE@gmail.com>
2025-05-16 00:33:29 -0700

    feat(disk): Add main pool, datasets, and drives to disko

--- machine/sun/disko.nix
+++ machine/sun/disko.nix
@@ -1,7 +1,13 @@
-{ config, lib, pkgs, ... } :
 {
-  disko.devices = let
-    mkZfsDisk = pool: device: {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  disko.devices =
+    let
+      mkZfsFastDisk = device: {
         type = "disk";
         inherit device;
         content = {
@@ -11,250 +17,274 @@
               size = "100%";
               content = {
                 type = "zfs";
-                inherit pool;
+                pool = "fast";
               };
             };
           };
         };
-    };
-    mkZfsFastDisk = mkZfsDisk "fast";
-    mkZfsMainDisk = mkZfsDisk "main";
-  in
-  {
-    disk = {
-      # Crucial MX500 500G
-      crucialMx500500g1 = {
+      };
+      mkZfsMainDisk = device: {
         type = "disk";
-        device = "/dev/disk/by-id/wwn-0x500a0751e88f3a5a";
+        inherit device;
         content = {
           type = "gpt";
           partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
+            swap = {
+              # 2G
+              start = "128";
+              end = "4194304";
               content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "nofail" ];
+                type = "swap";
+                randomEncryption = true;
               };
             };
             zfs = {
               size = "100%";
               content = {
                 type = "zfs";
-                pool = "zroot";
+                pool = "main";
               };
             };
           };
         };
       };
 
-      # Samsung 990 Pro 4T
-      samsung990Pro4t1 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144adbf";
-      samsung990Pro4t2 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144ae76";
+    in
+    {
+      disk = {
+        # Crucial MX500 500G
+        crucialMx500500g1 = {
+          type = "disk";
+          device = "/dev/disk/by-id/wwn-0x500a0751e88f3a5a";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "nofail" ];
+                };
+              };
+              zfs = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zroot";
+                };
+              };
+            };
+          };
+        };
 
-      # Ironwolf pro 18T
-      ironwolfPro18t1 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bd498e";
-      ironwolfPro18t2 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e45ff430";
-      ironwolfPro18t3 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5ace6c3";
-      ironwolfPro18t4 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5f75a6c";
-      ironwolfPro18t5 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bc90ab";
+        # Samsung 990 Pro 4T
+        samsung990Pro4t1 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144adbf";
+        samsung990Pro4t2 = mkZfsFastDisk "/dev/disk/by-id/nvme-eui.002538414144ae76";
 
+        # Ironwolf pro 18T
+        ironwolfPro18t1 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bd498e";
+        ironwolfPro18t2 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e45ff430";
+        ironwolfPro18t3 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5ace6c3";
+        ironwolfPro18t4 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5f75a6c";
+        ironwolfPro18t5 = mkZfsMainDisk "/dev/disk/by-id/wwn-0x5000c500e5bc90ab";
+
+      };
+      zpool = {
+        zroot = {
+          type = "zpool";
+          mode = {
+            topology = {
+              type = "topology";
+              vdev = [
+                {
+                  members = [ "crucialMx500500g1" ];
+                }
+              ];
+            };
+          };
+          options = {
+            # Workaround: cannot import 'zroot': I/O error in disko tests
+            cachefile = "none";
+            ashift = "12";
+          };
+          rootFsOptions = {
+            canmount = "off";
+            mountpoint = "none";
+            compression = "lz4";
+            acltype = "posixacl";
+            xattr = "sa";
+            atime = "off";
+          };
+          postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+          datasets = {
+            encrypted = {
+              type = "zfs_fs";
+              options = {
+                canmount = "off";
+                mountpoint = "none";
+                encryption = "aes-256-gcm";
+                keyformat = "passphrase";
+                keylocation = "prompt";
+              };
+            };
+            "encrypted/root" = {
+              type = "zfs_fs";
+              mountpoint = "/";
+            };
+            "encrypted/nix" = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options = {
+                compression = "zstd";
+              };
+            };
+          };
+        };
+        # };
+
+        fast = {
+          type = "zpool";
+          mode = {
+            topology = {
+              type = "topology";
+              vdev = [
+                {
+                  mode = "mirror";
+                  members = [
+                    "samsung990Pro4t1"
+                    "samsung990Pro4t2"
+                  ];
+                }
+              ];
+            };
+          };
+          options = {
+            ashift = "12";
+          };
+          rootFsOptions = {
+            canmount = "off";
+            mountpoint = "none";
+            atime = "off";
+            xattr = "sa";
+            compression = "lz4";
+          };
+          datasets = {
+            encrypted = {
+              type = "zfs_fs";
+              options = {
+                canmount = "off";
+                mountpoint = "none";
+                encryption = "aes-256-gcm";
+                keyformat = "hex";
+                keylocation = "file:///etc/zfs/key/fast/encrypted.hex";
+                # Use to bootstrap initial creation during install when root does not exist
+                # keyformat = "passphrase";
+                # keylocation = "prompt";
+              };
+            };
+            "encrypted/prometheus" = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "/var/lib/" + config.services.prometheus.stateDir;
+              };
+            };
+            game = {
+              type = "zfs_volume";
+              size = "1T";
+              options = {
+                volblocksize = "128K";
+              };
+              # content = {
+              #   type = "filesystem";
+              #   format = "ntfs";
+              # };
+            };
+          };
+        };
+
+        main = {
+          type = "zpool";
+          mode = {
+            topology = {
+              type = "topology";
+              vdev = [
+                {
+                  mode = "raidz2";
+                  members = [
+                    "ironwolfPro18t1"
+                    "ironwolfPro18t2"
+                    "ironwolfPro18t3"
+                    "ironwolfPro18t4"
+                    "ironwolfPro18t5"
+                  ];
+                }
+              ];
+            };
+          };
+          options = {
+            ashift = "12";
+          };
+          rootFsOptions = {
+            canmount = "off";
+            mountpoint = "none";
+            atime = "off";
+            xattr = "sa";
+            compression = "zstd";
+          };
+          datasets = {
+            encrypted = {
+              type = "zfs_fs";
+              options = {
+                canmount = "off";
+                mountpoint = "none";
+                encryption = "aes-256-gcm";
+                keyformat = "hex";
+                keylocation = "file:///etc/zfs/key/main/encrypted.hex";
+                # Use to bootstrap initial creation during install when root does not exist
+                # keyformat = "passphrase";
+                # keylocation = "prompt";
+              };
+            };
+            share = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "/mnt/main/share";
+                canmount = "on";
+                casesensitiveity = "insensitive";
+                acltype = "nfsv4";
+                aclmode = "restricted";
+              };
+            };
+            "share/public" = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "/mnt/main/share/public";
+                canmount = "on";
+              };
+            };
+            "share/public-write" = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "/mnt/main/share/public";
+                canmount = "on";
+                refquota = "1T";
+              };
+            };
+            media = {
+              type = "zfs_fs";
+              options = {
+                mountpoint = "/mnt/main/media";
+                canmount = "on";
+                acltype = "posix";
+              };
+            };
+          };
+        };
+      };
     };
-    zpool = {
-      zroot = {
-        type = "zpool";
-        mode = {
-          topology = {
-            type = "topology";
-            vdev = [
-              {
-                members = [ "crucialMx500500g1" ];
-              }
-            ];
-          };
-        };
-        options = {
-          # Workaround: cannot import 'zroot': I/O error in disko tests
-          cachefile = "none";
-          ashift = "12";
-        };
-        rootFsOptions = {
-          canmount = "off";
-          mountpoint = "none";
-          compression = "lz4";
-          acltype = "posixacl";
-          xattr = "sa";
-          atime = "off";
-        };
-        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
-        datasets = {
-          encrypted = {
-            type = "zfs_fs";
-            options = {
-              canmount = "off";
-              mountpoint = "none";
-              encryption = "aes-256-gcm";
-              keyformat = "passphrase";
-              keylocation = "prompt";
-            };
-          };
-          "encrypted/root" = {
-            type = "zfs_fs";
-            mountpoint = "/";
-          };
-          "encrypted/nix" = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            options = {
-              compression = "zstd";
-            };
-          };
-        };
-      };
-      # };
-
-      fast = {
-        type = "zpool";
-        mode = {
-          topology = {
-            type = "topology";
-            vdev = [
-              {
-                mode = "mirror";
-                members = [
-                  "samsung990Pro4t1"
-                  "samsung990Pro4t2"
-                ];
-              }
-            ];
-          };
-        };
-        options = {
-          ashift = "12";
-        };
-        rootFsOptions = {
-          canmount = "off";
-          mountpoint = "none";
-          atime = "off";
-          xattr = "sa";
-          compression = "lz4";
-        };
-        datasets = {
-          encrypted = {
-            type = "zfs_fs";
-            options = {
-              canmount = "off";
-              mountpoint = "none";
-              encryption = "aes-256-gcm";
-              keyformat = "hex";
-              keylocation = "file:///etc/zfs/key/fast/encrypted.hex";
-              # Use to bootstrap initial creation during install when root does not exist
-              # keyformat = "passphrase";
-              # keylocation = "prompt";
-            };
-          };
-          "encrypted/prometheus" = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "/var/lib/" + config.services.prometheus.stateDir;
-            };
-          };
-          game = {
-            type = "zfs_volume";
-            size = "1T";
-            options = {
-              volblocksize = "128K";
-            };
-            # content = {
-            #   type = "filesystem";
-            #   format = "ntfs";
-            # };
-          };
-        };
-      };
-      
-      main = {
-        type = "zpool";
-        mode = {
-          topology = {
-            type = "topology";
-            vdev = [
-              {
-                mode = "raidz2";
-                members = [
-                  "ironwolfPro18t1"
-                  "ironwolfPro18t2"
-                  "ironwolfPro18t3"
-                  "ironwolfPro18t4"
-                  "ironwolfPro18t5"
-                ];
-              }
-            ];
-          };
-        };
-        options = {
-          ashift = "12";
-        };
-        rootFsOptions = {
-          canmount = "off";
-          mountpoint = "none";
-          atime = "off";
-          xattr = "sa";
-          compression = "zstd";
-        };
-        datasets = {
-          encrypted = {
-            type = "zfs_fs";
-            options = {
-              canmount = "off";
-              mountpoint = "none";
-              encryption = "aes-256-gcm";
-              keyformat = "hex";
-              keylocation = "file:///etc/zfs/key/main/encrypted.hex";
-              # Use to bootstrap initial creation during install when root does not exist
-              # keyformat = "passphrase";
-              # keylocation = "prompt";
-            };
-          };
-          share = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "/mnt/main/share";
-              canmount = "on";
-              casesensitiveity = "insensitive";
-              acltype = "nfsv4";
-              aclmode = "restricted";
-            };
-          };
-          "share/public" = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "/mnt/main/share/public";
-              canmount = "on";
-            };
-          };
-          "share/public-write" = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "/mnt/main/share/public";
-              canmount = "on";
-              refquota = "1T";
-            };
-          };
-          media = {
-            type = "zfs_fs";
-            options = {
-              mountpoint = "/mnt/main/media";
-              canmount = "on";
-              acltype = "posix";
-            };
-          };
-        };
-      };
-    };
-  };
 
   boot.zfs.extraPools = [
     "fast"
