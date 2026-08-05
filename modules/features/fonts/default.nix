b12733cfba88d4dcf6bc8074fbe3e64ec7958105
Boris Nikulin <NikulinBE@gmail.com>
2026-08-05 05:38:32 -0700

    fix(fonts): Fix braille fonts for tty graphs like btop uses

--- modules/features/fonts/default.nix
+++ modules/features/fonts/default.nix
@@ -5,13 +5,39 @@
     fonts = {
       fontDir.enable = true;
 
-      enableDefaultPackages = true;
+      enableDefaultPackages = false;
       packages = with pkgs; [
         nerd-fonts.jetbrains-mono
+
+        # enableDefaultPackages without pkgs.freefont_ttf.
+        # https://discourse.nixos.org/t/display-braille-characters-btop-25-11-kde-plasma/75017
+        # https://github.com/NixOS/nixpkgs/blob/1559d3daa3ecc813a650b79375ea61b6741b8746/nixos/modules/config/fonts/packages.nix#L42
+        # GNU FreeFont has unusable braille which is used for TUI graphs like btop.
+        dejavu_fonts
+        gyre-fonts # TrueType substitutes for standard PostScript fonts
+        liberation_ttf
+        unifont
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-color-emoji
       ];
+
       fontconfig = {
+        enable = true;
         defaultFonts = {
-          monospace = [ "JetBrainsMono Nerd Font Propo" ];
+          monospace = [
+            "JetBrainsMono Nerd Font Propo"
+            "Noto Sans Mono"
+            "DejaVu Sans Mono"
+          ];
+          sansSerif = [
+            "Noto Sans"
+            "DejaVu Sans"
+          ];
+          serif = [
+            "Noto Serif"
+            "DejaVu Serif"
+          ];
         };
       };
     };
